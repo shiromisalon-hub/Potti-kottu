57ce66062f275ff7e17180114d1d0350586b738d
shiromisalon-hub <shiromisalon@gmail.com>
2026-04-23 19:21:34 +0530

    Add files via upload

---
+++ sunipy.py
@@ -0,0 +1,10 @@
+import turtle
+t = turtle.Turtle()
+t.speed(-999)
+color = ('red')
+for i in range(200):
+       t.forward(300-i)
+       t.left(170)
+       t.forward(300-i)
+       
+turtle.done()
